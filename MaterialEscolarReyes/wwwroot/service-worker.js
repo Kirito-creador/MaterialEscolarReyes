e2af3e4f1613d3185e0d6ac538a6fb5741b50e8a
Kirito-creador <leandromartinezreyes657@mail.com>
2026-07-02 17:18:58 -0400

    actualizacion service worker

--- MaterialEscolarReyes/wwwroot/service-worker.js
+++ MaterialEscolarReyes/wwwroot/service-worker.js
@@ -1,9 +1,12 @@
-﻿const CACHE_NAME = "reyes-cache-v1";
+﻿const CACHE_NAME = "reyes-cache-v2";
 
 const urlsToCache = [
-    "/",
     "/css/site.css",
-    "/js/site.js"
+    "/css/tienda.css",
+    "/js/site.js",
+    "/images/logo.png",
+    "/images/banner.png",
+    "/favicon.ico"
 ];
 
 self.addEventListener("install", event => {
@@ -11,11 +14,46 @@ self.addEventListener("install", event => {
         caches.open(CACHE_NAME)
             .then(cache => cache.addAll(urlsToCache))
     );
+
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+
+    event.waitUntil(
+
+        caches.keys().then(keys =>
+
+            Promise.all(
+                keys.map(key => {
+
+                    if (key !== CACHE_NAME)
+                        return caches.delete(key);
+
+                })
+            )
+
+        )
+
+    );
+
+    self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
+
+    if (event.request.mode === "navigate") {
+
+        event.respondWith(fetch(event.request));
+
+        return;
+    }
+
     event.respondWith(
+
         caches.match(event.request)
             .then(response => response || fetch(event.request))
+
     );
+
 });
